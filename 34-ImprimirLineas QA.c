b52c3097c8add38e6507fefafd33ec96b2e608f2
Zeusjonass <35499170+Zeusjonass@users.noreply.github.com>
2018-01-31 21:45:38 -0600

    Update 34-ImprimirLineas QA.c

--- 34-ImprimirLineas QA.c
+++ 34-ImprimirLineas QA.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /*
-Autor: César González
+Autor: CÃ©sar GonzÃ¡lez
 Entradas: limite
 Salidas: jContador
 Procedimiento general:
-Leer hasta dónde se va a contar.
-Se hace un ciclo que determina los valores que tendrá cada línea.
-Dentro del ciclo principal, uno secundario se efectuará por cada iteración del primero.
-El ciclo secundario contará desde 1 hasta el valor proporcionado por el primer ciclo.
-En cada iteración del segundo ciclo se va a imprimir el valor de su contador.
-Terminando el segundo ciclo se hace un salto de línea.
+Leer hasta dÃ³nde se va a contar.
+Se hace un ciclo que determina los valores que tendrÃ¡ cada lÃ­nea.
+Dentro del ciclo principal, uno secundario se efectuarÃ¡ por cada iteraciÃ³n del primero.
+El ciclo secundario contarÃ¡ desde 1 hasta el valor proporcionado por el primer ciclo.
+En cada iteraciÃ³n del segundo ciclo se va a imprimir el valor de su contador.
+Terminando el segundo ciclo se hace un salto de lÃ­nea.
  
 QA
 Entrada		Salida		Resultado
@@ -24,23 +24,23 @@ Entrada		Salida		Resultado
 		1 2		1 2
 5		1 2 3		1 2 3
 		1 2 3 4		1 2 3 4
-		1 2 3 4 5		1 2 3 4 5
+		1 2 3 4 5	1 2 3 4 5
  
 */
 int main(int argc, char *argv[]) {
 	int iContador, jContador, limite;
-	printf("Escribir el número hasta donde se va a contar\n");
+	printf("Escribir el nÃºmero hasta donde se va a contar\n");
 	/*Lectura*/
 	scanf("%d",&limite);
 	if(limite>0){
-		/*Ciclo que da el número de línea*/
+		/*Ciclo que da el nÃºmero de lÃ­nea*/
 		for(iContador=1; iContador<=limite;iContador++){
 			/*Ciclo que cuenta desde 1 hasta iContador*/
 			for(jContador=1;jContador<=iContador;jContador++){
 				/*Salidas*/
 				printf("%d ",jContador);
 			}
-			/*Salto de línea*/
+			/*Salto de lÃ­nea*/
 			printf("\n");
 		}
 	}
